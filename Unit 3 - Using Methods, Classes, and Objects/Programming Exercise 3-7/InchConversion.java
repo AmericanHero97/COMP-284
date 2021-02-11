7d5e50fe649b28a71596f26f62e17791387c3732
IakovTaranenko <IakovTaranenko@users.noreply.github.com>
2021-02-10 19:52:29 -0800

    Exercise 3-7 Completed
    Received 100% on Cengage

--- Unit 3 - Using Methods, Classes, and Objects/Programming Exercise 3-7/InchConversion.java
+++ Unit 3 - Using Methods, Classes, and Objects/Programming Exercise 3-7/InchConversion.java
@@ -4,11 +4,17 @@
 
 Programming Exercise 3-7
 
+Modify the class named InchConversion. Its main() method accepts a value in inches from a user at the keyboard, and in turn passes the entered value to two methods. One converts the value from inches to feet, and the other converts the same value from inches to yards.
+
 There are 12 inches in a foot and 3 feet in a yard.
 
-Create a class named InchConversion. Its main() method accepts a value in inches from a user at the keyboard, and in turn passes the entered value to two methods. One converts the value from inches to feet, and the other converts the same value from inches to yards.
-
 Each method displays the results with appropriate explanation.
+
+An example of the program is shown below:
+
+Enter inches >> 430
+430 inches is 35.833333333333336 feet
+430 inches is 11.944444444444445 yards
 
 */
 
@@ -16,14 +22,24 @@ import java.util.Scanner;
 
 public class InchConversion {
     public static void main (String args[]) {
-        // Write your code here
+        Scanner inputService = new Scanner(System.in);
+
+        System.out.println("Enter inches >> ");
+        int InchesInputted = inputService.nextInt();
+
+        convertToFeet(InchesInputted);
+        convertToYards(InchesInputted);
     }
 
     public static void convertToFeet(int inches) {
-        // Write your code here
+        final double INCHES_IN_FOOT = 12;
+        
+        System.out.println(inches + " inches is " + (inches / INCHES_IN_FOOT) + " feet");
     }
 
     public static void convertToYards(int inches) {
-        // Write your code here
+        final double INCHES_IN_YARD = 36;
+        
+        System.out.println(inches + " inches is " + (inches / INCHES_IN_YARD) + " yards");
     }
 }
