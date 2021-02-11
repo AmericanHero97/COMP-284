4495c20666475cfd487e22cdaf18cc3f64a8328c
IakovTaranenko <IakovTaranenko@users.noreply.github.com>
2021-02-10 18:55:29 -0800

    Exercise 3-6 Completed
    Received 100% on Cengage

--- Unit 3 - Using Methods, Classes, and Objects/Programming Exercise 3-6/BookstoreCredit.java
+++ Unit 3 - Using Methods, Classes, and Objects/Programming Exercise 3-6/BookstoreCredit.java
@@ -8,7 +8,13 @@ To encourage good grades, Hermosa High School has decided to award each student 
 
 In other words, a student with a 3.2 grade point average receives a $32.0 credit.
 
-Create an application that prompts a student for a name and grade point average, and then passes the values to a method (computeDiscount) that displays a descriptive message. The message uses the student’s name, echoes the grade point average, and computes and displays the credit, as follows: John, your GPA is 3.4, so your credit is $34.0.
+Create an application that prompts a student for a name and grade point average, and then passes the values to a method (computeDiscount) that displays a descriptive message. The message uses the student’s name, echoes the grade point average, and computes and displays the credit.
+
+An example of the program is shown below:
+
+Enter your name >> John
+Enter your gpa >> 3.4
+John, your  GPA is 3.4, so your credit is $34.0
 
 */
 
@@ -16,10 +22,20 @@ import java.util.Scanner;
 
 public class BookstoreCredit {
     public static void main (String args[]) {
-        // Write your code here
+        Scanner inputService = new Scanner(System.in);
+
+        System.out.println("Enter your name >>");
+        String NameGiven = inputService.nextLine();
+        System.out.println("Enter your gpa >>");
+        double GPAGiven = inputService.nextDouble();
+
+        computeDiscount(NameGiven, GPAGiven);
     }
 
     public static void computeDiscount(String name, double gpa) {
-        // Write your code here
+        final int GPA_CREDIT_MULTIPLIER = 10;
+        double Credit = gpa * GPA_CREDIT_MULTIPLIER;
+
+        System.out.print(name + ", your GPA is " + gpa + ", so your credit is $" + Credit);
     }
 }
