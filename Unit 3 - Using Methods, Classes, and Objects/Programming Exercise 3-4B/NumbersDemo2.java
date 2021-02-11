0280ff4a00e791689968181a5217b5bd46e46677
IakovTaranenko <IakovTaranenko@users.noreply.github.com>
2021-02-10 18:25:58 -0800

    Fixed formatting

--- Unit 3 - Using Methods, Classes, and Objects/Programming Exercise 3-4B/NumbersDemo2.java
+++ Unit 3 - Using Methods, Classes, and Objects/Programming Exercise 3-4B/NumbersDemo2.java
@@ -37,18 +37,15 @@ public class NumbersDemo2 {
       displayNumberPlusFive(num2);
       displayNumberSquared(num2);
    }
-   public static void displayTwiceTheNumber(int n)
-   {
+   public static void displayTwiceTheNumber(int n) {
       final int FACTOR = 2;
       System.out.println(n + " times " + FACTOR + " is " + (n * FACTOR));
    }
-   public static void displayNumberPlusFive(int n)
-   {
+   public static void displayNumberPlusFive(int n) {
       final int FACTOR = 5;
       System.out.println(n + " plus " + FACTOR + " is " + (n + FACTOR));
    }
-   public static void displayNumberSquared(int n)
-   {
+   public static void displayNumberSquared(int n) {
       System.out.println(n + " squared is " + (n * n)); 
    }
 }
