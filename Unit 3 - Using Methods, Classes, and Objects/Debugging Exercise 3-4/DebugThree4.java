75fdf414397ee81593a7bbf1a8351258b941d98d
IakovTaranenko <IakovTaranenko@users.noreply.github.com>
2021-02-12 00:10:42 -0800

    Debugging Exercise 3-4 Completed
    Received 100% on Cengage

--- Unit 3 - Using Methods, Classes, and Objects/Debugging Exercise 3-4/DebugThree4.java
+++ Unit 3 - Using Methods, Classes, and Objects/Debugging Exercise 3-4/DebugThree4.java
@@ -11,17 +11,19 @@ The file provided in the code editor to the right contains syntax and/or logic e
 // This program calculates tutition bills
 // as credits times rate per credit hour
 public class DebugThree4 {
-   public static void main(String args[]) {
+   public static void main(String[] args) {
       int myCredits = 13;
       int yourCredits = 17;
       double rate = 75.84;
-      System.out.println(My tuition:");
-      tuitionBill(myCredits, rate);
+
+      System.out.println("My tuition:");
+      TuitionBill(myCredits, rate);
+
       System.out.println("Your tuition:");
-      tuitionBill(yourCredits, myCredits);
+      TuitionBill(yourCredits, rate);
    }
 
    public static void TuitionBill(int c, double r) {
-      System.out.println("Total due  + (r / c));
+      System.out.println("Total due " + (r * c));
    }
 }
