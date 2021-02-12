62c4d816712c87d1c73a260e3843f58ceaff801f
IakovTaranenko <IakovTaranenko@users.noreply.github.com>
2021-02-12 00:04:49 -0800

    Debugging Exercise 3-2 Completed
    Received 100% on Cengage

--- Unit 3 - Using Methods, Classes, and Objects/Debugging Exercise 3-2/DebugThree2.java
+++ Unit 3 - Using Methods, Classes, and Objects/Debugging Exercise 3-2/DebugThree2.java
@@ -10,20 +10,18 @@ The file provided in the code editor to the right contains syntax and/or logic e
 
 // This application displays some math facts
 public class DebugThree2 {
-   public static void main(String args[]) {
+   public static void main(String[] args) {
       int a = 2, b = 5, c = 10;
       add(a, b);
       add(b, c);
       subtract(c, a);            
    }
 
-   public static void add(int a,  b) {
-      System.out.println("The sum of " + a +
-         " and " + a + " is " + (a + b));
+   public static void add(int a,  int b) {
+      System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
    }
 
-   public static void subtract(int a, b) {
-      System.out.println("The difference between  +
-        a + " and " + b + " is " +  (a * b));
+   public static void subtract(int a, int b) {
+      System.out.println("The difference between "  + a + " and " + b + " is " +  (a - b));
    }
 }
