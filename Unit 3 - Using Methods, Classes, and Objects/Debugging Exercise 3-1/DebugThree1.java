5a727a6c0e2a9353490645a0fcce4434a1122372
IakovTaranenko <IakovTaranenko@users.noreply.github.com>
2021-02-11 23:58:19 -0800

    Debugging Exercise 3-1 Completed
    Received 100% on Cengage

--- Unit 3 - Using Methods, Classes, and Objects/Debugging Exercise 3-1/DebugThree1.java
+++ Unit 3 - Using Methods, Classes, and Objects/Debugging Exercise 3-1/DebugThree1.java
@@ -10,18 +10,19 @@ The file provided in the code editor to the right contains syntax and/or logic e
 
 // This class calculates a waitperson's tip as 15% of the bill
 public class DebugThree1 {
-   public static void main(String args[]) {
+   public static void main(String[] args) {
       double myCheck = 50.00;
       double yourCheck = 19.95;
+
       System.out.println("Tips are");
       calcTip(myCheck);
-      calctip(yourcheck);
+      calcTip(yourCheck);
     }
-    public void calcTip(double bill)
-    {
+    public static void calcTip(double bill) {
        final double RATE = 0.15;
        double tip;
-       tip = check * RATE;
+       tip = bill * RATE;
+
        System.out.println("The tip should be at least " + tip);
     }
 }
