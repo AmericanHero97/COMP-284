908d618f60a65c1e8f76b9eb2268eea934019a6c
IakovTaranenko <IakovTaranenko@users.noreply.github.com>
2021-03-02 01:42:19 -0800

    Debugging Exercise 5-3 Completed
    Received 100% on Cengage

---
+++ Unit 5 - Making Decisions/Debugging Exercise 5-3/DebugFive3.java
@@ -0,0 +1,42 @@
+// DebugFive3.Java - Iakov Taranenko - 3/1/2021
+
+/* 
+
+Debugging Exercise 5-3
+
+The files provided in the code editor to the right contain syntax and/or logic errors. In each case, determine and fix the problem, remove all syntax and coding errors, and run the program to ensure it works properly.
+
+*/
+
+// DebugFive3.java
+// Determines whether item number on order is valid
+// Over 999 invalid
+// Less than 111 Invalid
+// Valid and less than 500 - Automotive Department
+// Valid and 500 or higher Housewares Department
+import java.util.Scanner;
+
+public class DebugFive3 {
+    public static void main (String args[]) {
+        int item;
+        String output;
+        final int LOW = 111;
+        final int HIGH = 999;
+        final int CUTOFF = 500;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter item number");
+        item = input.nextInt();
+
+        if (item < LOW) {
+            output = "Item number too low";
+        } else if (item > HIGH) {
+            output = "Item number too high";
+        } else if (item < CUTOFF) {
+            output = "Valid - in Automotive Department";
+        } else {
+            output = "Valid - Item in Housewares Department";
+        }
+        System.out.println(output);
+    }
+}
